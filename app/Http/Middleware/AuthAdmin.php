8aee6d23970dac87e216e43675f3b0a538c12350
morainstein <nichroberto@gmail.com>
2025-08-17 17:19:37 -0300

    feat: autenticação para admins, promoters e customers

---
+++ app/Http/Middleware/AuthAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Admin;
+use Closure;
+use Illuminate\Http\Request;
+use Laravel\Sanctum\PersonalAccessToken;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthAdmin
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $plainTextToken = $request->bearerToken();
+        $user = PersonalAccessToken::findToken($plainTextToken)->tokenable ?? false;
+
+        if (!$user || !$user instanceof Admin) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        return $next($request);
+    }
+}
