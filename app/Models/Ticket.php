361bda9dd8f18ec25c597e515aaa0d71b96730d8
morainstein <nichroberto@gmail.com>
2025-08-21 13:29:47 -0300

    feat: cria relacionamentos da model Ticket

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    protected $table = 'tickets';
+
+    protected $fillable = [
+        'batch_id',
+        'user_id',
+    ];
+
+    public function batch()
+    {
+        return $this->belongsTo(Batch::class, 'batch_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
