1f30c0d82f68f9614dd2a3c58981e24dabcc10a9
morainstein <nichroberto@gmail.com>
2025-08-21 17:09:49 -0300

    Fix: gera uuids para Tickets

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
+    use HasUuids;
+
     protected $table = 'tickets';
 
     protected $fillable = [
