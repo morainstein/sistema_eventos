141c6036b52eddafce2bf1c038a505821e6b12fe
morainstein <nichroberto@gmail.com>
2025-08-21 13:31:47 -0300

    feat: cria rota para compra de ingresso

--- routes/customerApiRoutes.php
+++ routes/customerApiRoutes.php
@@ -4,9 +4,10 @@ use App\Http\Controllers\CustomerController;
 use App\Http\Middleware\AuthCustomer;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/customer/register', [CustomerController::class, 'store']);
+Route::post('/customer', [CustomerController::class, 'store']);
 Route::post('/customer/login', [CustomerController::class, 'authenticate']);
 
 Route::middleware(AuthCustomer::class)->group(function () {
-
+    Route::post('/events/{event}/ticket', [CustomerController::class, 'buyTicket'])
+        ->whereUuid('event');
 });
