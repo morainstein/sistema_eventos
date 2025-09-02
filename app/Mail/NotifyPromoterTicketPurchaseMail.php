df38e7c4af8cd6451baa6ed936f29f582ea729f5
morainstein <nichroberto@gmail.com>
2025-09-02 13:37:12 -0300

    feat: notifica interessados (promotor e cliente) do pagamento concluído de um ingresso

---
+++ app/Mail/NotifyPromoterTicketPurchaseMail.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Customer;
+use App\Models\Event;
+use App\Models\Promoter;
+use App\Models\Ticket;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class NotifyPromoterTicketPurchaseMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(
+        public Promoter $promoter,
+        public Event $event,
+        public Ticket $ticket,
+        public Customer $customer,
+    )
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            to: $this->promoter->email,
+            subject: "{$this->event->title}: Ingresso comprado",
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'mail.notify-promoter-ticket-purchase',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
